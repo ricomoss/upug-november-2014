9512e3a0252b3c960bc98ce314c1fcdf74656a3f
Rico Cordova <rico@neutroninteractive.com>
2014-11-05 16:36:58 -0700

    Adding some tests.

--- django_example/example_project/example_app/tests.py
+++ django_example/example_project/example_app/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from fixtureless import Factory
 
-# Create your tests here.
+from views import FibNumView
+from models import FibHistory
+
+
+class SuperSnazzyViewTests(TestCase):
+    def setUp(self):
+        self.factory = Factory()
+        self.fib_num_view = FibNumView()
+
+    def test_fib(self):
+        expected_value = 0
+        self.assertEqual(self.fib_num_view.fib(0), expected_value)
+
+
+class SuperSnazzyModelTests(TestCase):
+    def setUp(self):
+        self.factory = Factory()
+
+    def test_sum_fib_nums(self):
+        initial = {
+            'fib_index': 0,
+            'fib_num': '1000',
+        }
+        fib_history = self.factory.create(FibHistory, initial)
+        self.assertEqual(fib_history.sum_fib_nums(), 1000)
